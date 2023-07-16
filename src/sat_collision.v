067fdbb8f8f7144e21100bbcf34ef8810d9996d1
YUART <zumikq78@gmail.com>
2023-07-16 11:17:01 +0200

    Refactor: sat_collision.v

--- src/sat_collision.v
+++ src/sat_collision.v
@@ -3,43 +3,60 @@ module pcoll2d
 import artemkakun.trnsfrm2d
 import arrays
 
+// check_collision checks if to polygons collide.
+// ATTENTION! Since the SAT algorithm is used, the polygons must be convex.
 pub fn check_collision(first_polygon_points []trnsfrm2d.Position, second_polygon_points []trnsfrm2d.Position) !bool {
-	polygons := [first_polygon_points, second_polygon_points]
+	return collides_on_all_axes(first_polygon_points, second_polygon_points)!
+		&& collides_on_all_axes(second_polygon_points, first_polygon_points)!
+}
 
-	for polygon in polygons {
-		for point_index in 0 .. polygon.len {
-			// Sure! The expression i2 = (i1 + 1) % len(polygon) is used to ensure that we correctly cycle around the vertices of the polygon when we're calculating the edges.
-			// In a polygon with nn vertices, the edges are formed by pairs of vertices: (0,1),(1,2),(2,3),…,(n−2,n−1),(n−1,0)(0,1),(1,2),(2,3),…,(n−2,n−1),(n−1,0). The last pair, (n−1,0)(n−1,0), closes the polygon by connecting the last vertex with the first one.
-			// The modulus operation (%) in Python gives the remainder of the division. When the index i1 reaches the end of the list (i.e., i1 becomes n-1), i1 + 1 would be n, which is out of bounds for the list of vertices. By doing (i1 + 1) % len(polygon), we ensure that the result wraps around to 0 when i1 is n-1. So, i2 becomes 0 when i1 is n-1, giving us the last pair (n−1,0)(n−1,0) and ensuring that we correctly cycle around the polygon.
-			// So, i2 = (i1 + 1) % len(polygon) is a neat Python trick that allows us to iterate over pairs of vertices in a cyclic manner.
-			next_vertex_index := (point_index + 1) % polygon.len
+fn collides_on_all_axes(polygon_points []trnsfrm2d.Position, another_polygon_points []trnsfrm2d.Position) !bool {
+	polygon_points_count := polygon_points.len
 
-			p1 := polygon[point_index]
-			p2 := polygon[next_vertex_index]
+	for point_index in 0 .. polygon_points_count {
+		// HACK:
+		// The expression next_vertex_index = (point_index + 1) % polygon_points.len is used to ensure
+		// that we correctly cycle around the vertices of the polygon when we're calculating the edges.
+		// In a polygon with n vertices, the edges are formed by pairs of vertices:
+		// (0,1),(1,2),(2,3),…,(n−2,n−1),(n−1,0)(0,1),(1,2),(2,3),…,(n−2,n−1),(n−1,0).
+		// The last pair, (n−1,0)(n−1,0), closes the polygon by connecting the last vertex with the first one.
+		// The modulus operation (%) gives the remainder of the division.
+		// When the index point_index reaches the end of the list (i.e., point_index becomes n-1),
+		// point_index + 1 would be n, which is out of bounds for the list of vertices.
+		// By doing (point_index + 1) % polygon_points.len, we ensure that the result wraps around to
+		// 0 when point_index is n-1. So, next_vertex_index becomes 0 when point_index is n-1,
+		// giving us the last pair (n−1,0)(n−1,0) and ensuring that we correctly cycle around the polygon.
+		next_vertex_index := (point_index + 1) % polygon_points_count
 
-			edge := trnsfrm2d.Vector{
-				x: p2.x - p1.x
-				y: p2.y - p1.y
-			}
+		first_edge_point := polygon_points[point_index]
+		second_edge_point := polygon_points[next_vertex_index]
 
-			normal := trnsfrm2d.Vector{
-				x: -edge.y
-				y: edge.x
-			}
+		edge := trnsfrm2d.Vector{
+			x: second_edge_point.x - first_edge_point.x
+			y: second_edge_point.y - first_edge_point.y
+		}
 
-			min1, max1 := project_polygon(first_polygon_points, normal)!
-			min2, max2 := project_polygon(second_polygon_points, normal)!
+		normal := trnsfrm2d.Vector{
+			x: -edge.y
+			y: edge.x
+		}
 
-			if max1 < min2 || max2 < min1 {
-				return false
-			}
+		polygon_on_normal_min, polygon_on_normal_max := project_polygon_on_axis(polygon_points,
+			normal)!
+
+		another_polygon_on_normal_min, another_polygon_on_normal_max := project_polygon_on_axis(another_polygon_points,
+			normal)!
+
+		if polygon_on_normal_max < another_polygon_on_normal_min
+			|| another_polygon_on_normal_max < polygon_on_normal_min {
+			return false
 		}
 	}
 
 	return true
 }
 
-fn project_polygon(polygon_points []trnsfrm2d.Position, normal trnsfrm2d.Vector) !(f64, f64) {
+fn project_polygon_on_axis(polygon_points []trnsfrm2d.Position, normal trnsfrm2d.Vector) !(f64, f64) {
 	mut dot_products := []f64{}
 
 	for point in polygon_points {
